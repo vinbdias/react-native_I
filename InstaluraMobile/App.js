49b435934043832ba45e1b73c2e0eeee23df4c59
vinbdias <vintheman@gmail.com>
2019-02-22 11:04:24 -0300

    Até aula 02, atividade 01.

--- InstaluraMobile/App.js
+++ InstaluraMobile/App.js
@@ -8,7 +8,18 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Dimensions
+} from 'react-native';
+
+
+const width = Dimensions.get('screen').width,
+      height = Dimensions.get('screen').height;
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -21,30 +32,12 @@ type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+      <View> 
+        <Text>vinicius</Text>
+        <Image source={require('./resources/img/alura.jpg')} 
+          style={{ width, height }} />
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
